416234d2db3f763337456c49375c9c163c51390c
artemtret <artem.tretykov74@gmail.com>
2023-09-30 19:15:25 +0300

    adding new handlers

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,12 +1,15 @@
+from .main_menu import dp
 from .start_cmd import dp
 from .fast_answer_questions import dp
-from .main_menu import dp
 from .admin import dp
 from .new_keys import dp
-from .check_keys import dp
-from .autopilot import dp
-from .check_balance import dp
+from .zora_autopilot import dp
 from .check_gwei import dp
+from .change_network import dp
+from .my_account import dp
+from .deposit_balance import dp
+
 from .restart import dp
+
 
 __all__ = ["dp"]
