487ba41823b675d992a3ea3c549b8024ef557db0
artemtret <artem.tretykov74@gmail.com>
2023-10-08 13:38:49 +0300

    update code

--- app/utils/Randomiser.py
+++ app/utils/Randomiser.py
@@ -25,25 +25,25 @@ class Randomiser:
 
     @staticmethod
     def random_bridge():
-        return random.randint(1, 5)
+        return random.randint(45, 90)
     @staticmethod
     def random_bridge_after():
-        return random.randint(1, 5)
+        return random.randint(90, 120)
     @staticmethod
     def random_contract():
-        return random.randint(1, 5)
+        return random.randint(120, 150)
     @staticmethod
     def random_contract_after():
-        return random.randint(1, 5)
+        return random.randint(100, 200)
     @staticmethod
     def random_warm_up():
-        return random.randint(1, 5)
+        return random.randint(90, 120)
     @staticmethod
     def random_warm_up_after():
-        return random.randint(1, 5)
+        return random.randint(500, 600)
     @staticmethod
     def random_mint():
-        return random.randint(1, 5)
+        return random.randint(60, 80)
     @staticmethod
     def random_mint_after():
-        return random.randint(1, 5)
+        return random.randint(300, 500)
