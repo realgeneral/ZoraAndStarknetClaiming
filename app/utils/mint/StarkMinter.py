34722f9f43a0f0a72b8e9d930216b591d6182e3d
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-10-11 21:37:43 +0300

    starkminter upd

--- app/utils/mint/StarkMinter.py
+++ app/utils/mint/StarkMinter.py
@@ -1,12 +1,10 @@
 import random
 from random import uniform, randint
 
+from src.utils.Client import Client
+from src.utils.Info import ContractInfo, TokenAmount
+from src.config.logger import logger
 from starknet_py.contract import Contract
-
-from app.utils.stark_utils.Client import Client
-from app.utils.stark_utils.Info import ContractInfo, TokenAmount
-from app.logs import logging as logger
-
 
 
 class Minter:
@@ -21,10 +19,15 @@ class Minter:
 
             logger.info(f"[{self.client.address_to_log}] Minting nft [StarkVerse NFT]")
 
-            tx_hash = await self.client.send_transaction(interacted_contract=contract,
-                                                         function_name='publicMint',
-                                                         to=self.client.address
-                                                         )
+            # tx_hash = await self.client.send_transaction(interacted_contract=contract,
+            #                                              function_name='publicMint',
+            #                                              to=self.client.address
+            #                                              )
+
+            tx_hash = await self.client.call(interacted_contract_address=contract_address,
+                                             calldata=[self.client.address],
+                                             selector_name='publicMint')
+
             if tx_hash:
                 logger.info(
                     f"[{self.client.address_to_log}] Successfully minted [StarkVerse NFT]")
@@ -49,11 +52,13 @@ class Minter:
 
             max_fee = TokenAmount(amount=float(uniform(0.0007534534534, 0.001)))
 
-            prepared_call = contract.functions["mint"].prepare(id, max_fee=int(max_fee.Wei * (1 + randint(15, 30) / 100)))
-            await prepared_call.invoke()
+            tx_hash = await self.client.call(interacted_contract_address=contract_address,
+                                             calldata=[id],
+                                             selector_name='mint')
 
-            logger.info(f"[{self.client.address_to_log}] Successfully minted [Starknet.id Identity NFT]")
-            return True
+            if tx_hash:
+                logger.info(f"[{self.client.address_to_log}] Successfully minted [Starknet.id Identity NFT]")
+                return True
         except Exception as err:
             if "Contract not found" in str(err):
                 logger.error(f"[{self.client.address_to_log}] Seems contract (address) is not deployed yet because did it not have any txs before [Starknet.id Identity NFT]")
