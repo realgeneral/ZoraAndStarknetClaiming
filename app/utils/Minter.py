dfc2b23afffd5cf4a0c3cd64ef9ab636bc45af95
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-10-29 14:30:25 +0300

    new mints added to mintfun func

--- app/utils/Minter.py
+++ app/utils/Minter.py
@@ -7,7 +7,7 @@ import web3.exceptions as ex3
 
 from web3 import Web3
 from app.logs import logging as logger
-from app.utils.configs.config import rpcs, nft_contract_abi, nft_ZoraCreator_contract_abi, ZoraNFTCreator_contract_abi, JSONExtensionRegistry_contract_abi, blocks_contract_abi, mint_contract_abi
+from app.utils.configs.config import rpcs, nft_contract_abi, nft_ZoraCreator_contract_abi, ZoraNFTCreator_contract_abi, JSONExtensionRegistry_contract_abi, blocks_contract_abi, mint_contract_abi, yassin_contract_abi
 
 
 class Minter:
@@ -365,7 +365,10 @@ class Minter:
 
     async def mintfun(self, nft_address, nft_name: str):
 
-        web3 = Web3(Web3.HTTPProvider(rpcs["zora"]))
+        web3 = Web3(Web3.HTTPProvider(rpcs["zora"], request_kwargs={
+                                       'proxies': {'https': "http://pnorwyha:snmfocltb81h@216.173.109.154:6385",
+                                                   'http': "http://pnorwyha:snmfocltb81h@216.173.109.154:6385"}}
+                                      ))
 
         logger.info(f"Successfully connected to {rpcs['zora']}")
 
@@ -489,9 +492,42 @@ class Minter:
                         logger.error(f"⛔ Something went wrong while minting: {err}, {nft_address}")
                         return "❌ Something went wrong"
 
-            elif nft_name == "SpiderMan in Vintage Vibe":
-                contract = web3.eth.contract(address=Web3.to_checksum_address(nft_address), abi=mint_contract_abi)
-                pass
+            elif nft_name == "Yassin Art on Zora":
+                contract = web3.eth.contract(address=Web3.to_checksum_address(nft_address), abi=yassin_contract_abi)
+                mint_tx = contract.functions.Mint(
+                    11  # 30 and 500 available
+                ).build_transaction({
+                    'from': web3.to_checksum_address(wallet_address),
+                    'value': web3.to_wei(0.00007754, 'ether'),
+                    'gas': 150000,
+                    'nonce': web3.eth.get_transaction_count(wallet_address),
+                    'maxPriorityFeePerGas': web3.to_wei(0.005, 'gwei'),
+                    'maxFeePerGas': web3.to_wei(0.005, 'gwei')
+                })
+
+                signed_mint_tx = web3.eth.account.sign_transaction(mint_tx, self.pk)
+                raw_mint_tx_hash = web3.eth.send_raw_transaction(signed_mint_tx.rawTransaction)
+                mint_tx_hash = web3.to_hex(raw_mint_tx_hash)
+
+                logger.info(f"Mint tx hash: {mint_tx_hash}")
+
+                for _ in range(5):
+                    await asyncio.sleep(5)
+                    try:
+                        mint_tx_receipt = web3.eth.wait_for_transaction_receipt(raw_mint_tx_hash, timeout=300)
+
+                        if mint_tx_receipt.status == 1:
+                            logger.info(f"✅ Transaction: https://explorer.zora.energy/tx/{mint_tx_hash}, {nft_address}")
+                            return "✅"
+                        else:
+                            logger.error(f"⛔ Something went wrong while minting, {nft_address}")
+                            return "❌ Something went wrong"
+                    except ex3.TransactionNotFound as err:
+                        logger.error(f"⛔ Something went wrong while minting: {err}, {nft_address}")
+                        continue
+                    except Exception as err:
+                        logger.error(f"⛔ Something went wrong while minting: {err}, {nft_address}")
+                        return "❌ Something went wrong"
 
         except Exception as err:
             if "insufficient funds" and "have" in str(err):
@@ -506,3 +542,10 @@ class Minter:
             else:
                 logger.error(f"TOTAL something went wrong: {err}, {nft_address}")
                 return "❌ Something went wrong"
+
+
+#if __name__ == "__main__":
+    #asyncio.run(minter.mintfun("0x1F781d47cD59257D7AA1Bd7b2fbaB50D57AF8587", nft_name="Blocks"))
+    #asyncio.run(minter.mintfun("0x3a577c80f5834B0150DEFEa2AB71Ae7AEF5f463d", nft_name="The Mancer"))
+    #asyncio.run(minter.mintfun("0xbC2cA61440fAF65a9868295Efa5d5D87c55B9529", nft_name="sqr(16)"))
+    #asyncio.run(minter.mintfun("0xD425b16d3eF1a1ec0AB9b3f6CBeFD5Fe6BE42259", nft_name="Yassin Art on Zora"))
